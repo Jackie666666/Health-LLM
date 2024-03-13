f83a4a0684e3ec95416ee5c9dd75291c434ca30f
Yu Bin, Kim <45308022+ybkim95@users.noreply.github.com>
2024-03-12 21:14:19 -0400

    Update finetune.sh

--- finetune.sh
+++ finetune.sh
@@ -4,7 +4,7 @@ data_path="data/finetune_data.json"
 output_path="medalpaca-13b"
 
 torchrun --nproc_per_node=4 --master_port=2023 medalpaca/train.py \
-    --model "llama-7b-bf" \
+    --model "llama-13b-bf" \
     --data_path "$data_path" \
     --output_dir "$output_path" \
     --train_in_8bit False \
